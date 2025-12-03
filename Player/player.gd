486d7bfabb703049d6f8cc630659932aee0fe362
Jonathan Marien <90732393+Mightysmallfry@users.noreply.github.com>
2025-12-03 15:27:03 -0800

    Updated Jump

--- Player/player.gd
+++ Player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 @export var SPEED : float = 200.0
-@export var JUMP_VELOCITY : float = -400.0
+@export var JUMP_FORCE : float = -350.0
 
 var is_jumping : bool = false
 var jump_time : float = 0.0
@@ -12,30 +12,15 @@ var max_jump_time : float = 10.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		if jump_time < max_jump_time:
-			velocity.y += JUMP_VELOCITY * delta
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		is_jumping = true
-		jump_time = 0.0
-
-	if Input.is_action_pressed("jump") and is_jumping:
-		if jump_time < max_jump_time:
-			velocity += get_gravity() * delta
-			jump_time += delta
-		else:
-			is_jumping = false
-
-	if Input.is_action_just_released("ui_accept"):
-		is_jumping = false
-
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# ---------
+	# Handle Jump
+	# ---------
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_FORCE
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -45,6 +30,10 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	# -------
+	# Handle Animation
+	# -------
+	
 	if velocity.x == 0:
 		sprite.play("idle")
 	else:
